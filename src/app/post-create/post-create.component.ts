5236472108851a76a2aaafc83db57335c51795a2
CrisGE00 <crisge10@hotmail.es>
2025-05-09 16:31:51 +0200

    added forms module

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css'
 })
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = '';
 
+  onAddPost(){
+    this.newPost = this.enteredValue;
+  }
 }
